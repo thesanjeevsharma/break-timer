dfd9ac90fb7db8ba9497fde431208c02264bbae6
Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>
2020-09-27 16:42:07 +0530

    feat: icons, menu and app window details

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
+process.env.NODE_ENV = "development";
 const { app, BrowserWindow, Menu } = require("electron");
 
+const isMac = process.platform === "darwin";
+
+let win;
+
 const createWindow = () => {
-  const win = new BrowserWindow({
+  win = new BrowserWindow({
     width: 500,
     height: 400,
+    title: "Break Timer",
+    resizable: false,
+    transparent: true,
+    icon: "icons/logo.png",
     webPreferences: {
       nodeIntegration: true,
     },
   });
   win.loadFile("index.html");
+
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+
+  if (isMac) {
+    app.dock.setMenu(dockMenu);
+  }
 };
 
 const dockMenu = Menu.buildFromTemplate([
@@ -20,8 +36,58 @@ const dockMenu = Menu.buildFromTemplate([
   },
 ]);
 
-if (process.platform === "darwin") {
-  app.dock.setMenu(dockMenu);
-}
+const template = [
+  ...(isMac
+    ? [
+        {
+          label: app.name,
+          submenu: [
+            {
+              label: "Star on GitHub",
+              click: async () => {
+                const { shell } = require("electron");
+                await shell.openExternal(
+                  "https://github.com/thesanjeevsharma/break-timer"
+                );
+              },
+            },
+          ],
+        },
+      ]
+    : []),
+  {
+    label: "File",
+    submenu: [
+      {
+        label: "Exit",
+        accelerator: "Esc",
+        click() {
+          win.hide();
+        },
+      },
+      {
+        label: "Quit",
+        accelerator: isMac ? "Command+Q" : "Ctrl+Q",
+        click() {
+          app.quit();
+        },
+      },
+    ],
+  },
+  {
+    role: "help",
+    submenu: [
+      {
+        label: "GitHub",
+        click: async () => {
+          const { shell } = require("electron");
+          await shell.openExternal(
+            "https://github.com/thesanjeevsharma/break-timer"
+          );
+        },
+      },
+    ],
+  },
+];
 
 app.whenReady().then(createWindow);
