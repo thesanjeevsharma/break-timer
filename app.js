0e31e09030f38453262acce022e1d865f8113195
Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>
2020-09-27 16:02:08 +0530

    feat: macOS dock menu

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, Menu } = require("electron");
 
 const createWindow = () => {
   const win = new BrowserWindow({
@@ -10,5 +10,18 @@ const createWindow = () => {
   });
   win.loadFile("index.html");
 };
+
+const dockMenu = Menu.buildFromTemplate([
+  {
+    label: "Quit",
+    click() {
+      app.quit();
+    },
+  },
+]);
+
+if (process.platform === "darwin") {
+  app.dock.setMenu(dockMenu);
+}
 
 app.whenReady().then(createWindow);
